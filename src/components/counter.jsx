146103a9920b3d31b49f2e5a51d750e630eee5d7
[Sathiya15l] <[sathiyalakshmive@gmail.com]>
2024-07-05 21:27:17 +0530

    ComponentsPushed

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+/*class Counter extends Component {
+    
+    render() { 
+        return (<h1>Hello World</h1>);
+    }
+}*/
+
+function Counter(){
+    return(
+        <h1>Hello World </h1>
+    );
+}
+ 
+export default Counter;
